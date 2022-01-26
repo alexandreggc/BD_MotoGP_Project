61d55705720efad2d106f8a1b56fccca1d2d085c
Henrique-O-S <76045716+Henrique-O-S@users.noreply.github.com>
2022-01-26 02:24:15 +0000

    Add files via upload

--- queries/int5.sql
+++ queries/int5.sql
@@ -2,6 +2,6 @@
 .headers on
 .nullvalue NULL
 
-SELECT nome
+SELECT (SELECT nome FROM Equipa WHERE idEquipa = Piloto.idEquipa) AS nomeEquipa, Colaborador.nome, (SELECT posicaoInicial FROM Grid WHERE idColaborador = Colaborador.idColaborador) AS posicaoInicial
 FROM (Colaborador JOIN Piloto ON Colaborador.idColaborador = Piloto.idColaborador)
 WHERE Colaborador.idColaborador IN (SELECT idColaborador FROM Grid WHERE idCorrida = (SELECT idCorrida FROM Corrida WHERE nome = "Grande Premio de Franca") AND posicaoFinal = 1);
